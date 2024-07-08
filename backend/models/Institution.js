d92a208958e2116aed18bd6032eedd16d219ccb3
Marco <mrc.xausa@gmail.com>
2024-07-08 23:01:46 +0200

    Added backend and client
    Added backend and react client

---
+++ backend/models/Institution.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const institutionSchema = new Schema(
+  {
+    name: String,
+    vat: String,
+    email: String,
+    password: String,
+    services: [String],
+  },
+  {
+    collection: "institutions",
+  }
+);
+
+const Institution = mongoose.model("Institution", institutionSchema);
+
+module.exports = Institution;
---
+++ client/src/components/Subscribe/Registerinstitution.jsx
@@ -0,0 +1,117 @@
+require('dotenv').config();
+
+
+function RegisterInstitution({setDisplay}){
+
+
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        
+        let name =e.target.name.value;
+        let email =e.target.email.value;
+        let vat =e.target.vat.value;
+        let password =e.target.password.value;
+
+        var toBeSent={
+            name:name,
+            email:email,
+            vat:vat,
+            password:password,
+            //address:accounts[0]
+        };
+
+        fetch("http://localhost:3000/v1/registerinstitution",{
+            method:'POST',
+            headers: { "Content-Type": "application/json" },
+            body:JSON.stringify(toBeSent)
+        }) 
+        .then(response => {
+            return response.json();
+        })
+        // .then(async (response) => {
+        //     if(response.stored===false)
+        //         alert("Institution has not been created. Error:"+response.error);
+        //     else{               
+        //         const owner = web3.eth.accounts.privateKeyToAccount(process.env.REACT_APP_CONTRACT_OWNER_PRVATEKEY);
+
+        //         web3.eth.accounts.wallet.add(owner);
+                
+        //         await contract.methods.addInstitution(accounts[0]).send({ 
+        //             from: owner.address,
+        //             gas: 100000
+        //         })
+        //         .then(async()=>{
+        //             let authorized=await contract.methods.isAuthorizedInstitution(accounts[0]).call({ from: owner.address});
+        //             console.log("Institution authorized="+authorized);
+        //             if(authorized)
+        //                 alert("Institution created!");
+        //             else{
+        //                 alert("problem with the creation of the institution");
+        //                 fetch("http://localhost:3000/v1/rollbackinstitution",{
+        //                     method:'POST',
+        //                     headers: { "Content-Type": "application/json" },
+        //                     body:JSON.stringify({vat:vat})
+        //                 });
+        //             }
+        //         })
+        //         .catch ((error)=> {
+        //             alert('an error occurred');
+        //             console.log('error: '+error);
+        //             fetch("http://localhost:3000/v1/rollbackinstitution",{
+        //                 method:'POST',
+        //                 headers: { "Content-Type": "application/json" },
+        //                 body:JSON.stringify({vat:vat})
+        //             });
+        //         });
+
+                
+        //     }
+        // })
+
+
+
+
+        
+    }
+
+    function handleClick(e){
+        e.preventDefault(); 
+        setDisplay(true);
+    }
+
+    return (
+        <>
+         <form className="user-institution-register-form" onSubmit={handleSubmit}>
+            <h1>Insert your information</h1>
+            <label >Institution name: </label>
+            <input type="text" name="name"  />
+            <br />
+            <br />
+            <label >VAT code: </label>
+            <input type="text" name="vat"  />
+            <br />
+            <br />
+            <label >email: </label>
+            <input type="text" name="email"  />
+            <br />
+            <br />
+            <label >password: </label>
+            <input type="password" name="password"  />
+            <br />
+            <br />
+            <button type="submit">register</button>
+            
+            <br />
+            <br />
+            <u><b><label>Pay attention to the account selected on metamask,<br /> it will be bounded to the institution</label></b></u>
+            <br />
+            <br />
+            <button onClick={handleClick}>back</button>
+            
+        </form>
+        </>
+    );
+}
+
+export default RegisterInstitution;
