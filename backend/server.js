7cf8b36d6c20b6f376fad244d1b8a1c0e62cf658
AleSommaruga <alessandrosommarug@gmail.com>
2024-07-14 22:36:19 +0200

    chore: update gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const app=require("./app")
-const mongoose = require('mongoose')
+const app = require("./app");
+const mongoose = require("mongoose");
 
 var port = process.env.PORT || 8080;
 
-
-var db = mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
-    console.log("Connected to Database"), 
-    app.listen(port, () => { console.log(`Server listening`); })
-}).catch(()=> {
-    console.log("Database connection Error")
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to Database"),
+      app.listen(port, () => {
+        console.log(`Server listening`);
+      });
+  })
+  .catch(() => {
+    console.log("Database connection Error");
+  });
