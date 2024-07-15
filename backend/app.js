ebfda1e8947b5d5eb079ce2fe4dd61ca8e98fa72
AleSommaruga <alessandrosommarug@gmail.com>
2024-07-15 23:01:15 +0200

    chore: add logs

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,13 @@ const routerv1 = require("./Router1.js");
 const app = express();
 const cors = require("cors");
 
+app.use((req, res, next) => {
+  console.log(
+    `Received request: ${req.method} ${req.url} from origin ${req.headers.origin}`
+  );
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
