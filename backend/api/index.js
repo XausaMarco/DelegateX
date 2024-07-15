574ec39c414f6734c9ba607b1dc480de68dc8fb2
AleSommaruga <alessandrosommarug@gmail.com>
2024-07-15 23:17:32 +0200

    fix: connect to db

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,16 +7,16 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-  mongoose
-    .connect(process.env.MONGODB_CONNECTION_STRING)
-    .then(() => {
-      console.log("Connected to Database");
-    })
-    .catch(() => {
-      console.log("Database connection Error");
-    });
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to Database"),
+      app.listen(port, () => {
+        console.log(`Server listening on port ${port}`);
+      });
+  })
+  .catch(() => {
+    console.log("Database connection Error");
+  });
 
 module.exports = app;
