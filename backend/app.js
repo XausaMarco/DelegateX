6471a5ab61b9e2c5729a6497378736cf99f8edd2
AleSommaruga <alessandrosommarug@gmail.com>
2024-07-15 22:53:58 +0200

    fix: cors 4 (tentative)

--- backend/app.js
+++ backend/app.js
@@ -3,23 +3,7 @@ const routerv1 = require("./Router1.js");
 const app = express();
 const cors = require("cors");
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (
-        origin &&
-        origin.startsWith("https://delegate-x-frontend.vercel.app/")
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/v1", routerv1);
